31853a3790e90d35b8a084d5f28ecc1739f8b865
Fogus <fogusw@yandex.ru>
2021-07-25 23:03:05 +0300

    Add Fixtures

---
+++ src/DataFixtures/CartFixture.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Cart;
+use App\Entity\CartProduct;
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Yaml\Yaml;
+
+class CartFixture extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        $fixtures = Yaml::parse(file_get_contents(__DIR__ . '/Resources/carts.yaml'));
+        foreach ($fixtures['carts'] as $cartFixture) {
+            $cart = new Cart();
+
+            foreach ($cartFixture['products'] as $cartProductFixture) {
+                /** @var Product $product */
+                $product = $this->getReference('product-' . $cartProductFixture['name']);
+
+                $cart->addProduct($product, $cartProductFixture['count']);
+            }
+
+            $manager->persist($cart);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            ProductFixture::class,
+        ];
+    }
+}
---
+++ src/DataFixtures/ProductFixture.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Product;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Yaml\Yaml;
+
+class ProductFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $fixtures = Yaml::parse(file_get_contents(__DIR__ . '/Resources/products.yaml'));
+        foreach ($fixtures['products'] as $productFixture) {
+            $product = new Product();
+            $product->setName($productFixture['name']);
+            $product->setTitle($productFixture['title']);
+            $product->setPriceCents($productFixture['priceCents']);
+
+            $manager->persist($product);
+            $this->addReference('product-' . $productFixture['name'], $product);
+        }
+
+        $manager->flush();
+    }
+}
